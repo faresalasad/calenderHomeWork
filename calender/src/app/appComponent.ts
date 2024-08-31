8a85ceefccc58adce4da49c6342c9036d73ea7db
sham <sham2sham2024@hotmail.com>
2024-09-01 01:01:46 +0300

    add project

---
+++ calender/src/app/appComponent.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: "./app/appComponent",
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'calender';
+}
